b2aaeeba4313e68917fc8711d1330431e8760c29
Jeremy Bennett <25406708+JaxJafinPapau@users.noreply.github.com>
2020-10-11 17:34:02 -0700

    Adds basic model test for post

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe "Post", type: :model do
+  describe "validations" do
+    it {is_expected.to validate_presence_of(:title)}
+    it {is_expected.to validate_presence_of(:body)}
+    it {is_expected.to validate_presence_of(:author)}
+  end
+
+  describe "ralationships" do
+    it {is_expected.to belong_to(:author)}
+  end
+end
